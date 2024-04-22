d02d8b1310ad56b34eafbd698b75f0a03403ea8b
Grzegorz Prabucki <132297553+Grzegorztestowy@users.noreply.github.com>
2024-04-22 21:05:24 +0200

    Fixed bugs found by the mentor

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
 import "./style.css";
-  
-const Form = (props) => (
-    <form className="form">
-        <input className="form__input" type="text" placeholder="Co jest do zrobienia?" />
-        <button className="form__button">Dodaj zadanie</button>
-    </form>
+const Form = () => (
+  <form className="form">
+    <input
+      className="form__input"
+      type="text"
+      placeholder="Co jest do zrobienia?"
+    />
+    <button className="form__button">Dodaj zadanie</button>
+  </form>
 );
 
 export default Form;
