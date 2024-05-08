af092a91e11f86eb3422d118c05fb4d2806e0c9d
Grzegorz Prabucki <132297553+Grzegorztestowy@users.noreply.github.com>
2024-05-08 21:05:50 +0200

    trying to find the show/hide complete error and modifying other elements of the page.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
@@ -7,10 +7,10 @@ import Header from "./Header";
 import Container from "./Container";
 
 function App() {
-  const[hideDone, setHideDone] = useState(false);
-  const[tasks, setTasks] = useState([
+  const [hideDone, setHideDone] = useState(false);
+  const [tasks, setTasks] = useState([
     { id: 1, content: "przejść na Reacta", done: false },
-    { id: 2, content: "zjeść kolację", done: true },
+    { id: 2, content: "zjeść kolacje", done: true },
   ]);
 
   const toggleHideDone = () => {
@@ -19,24 +19,65 @@ function App() {
 
   const removeTask = (id) => {
     setTasks(tasks => tasks.filter(task => task.id !== id));
+  };
 
-  }
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, done: !task.done };
+      }
+
+      return task;
+    }));
+  };
+
+  const setAllDone = () => {
+    setTasks(tasks => tasks.map(task => ({
+      ...task,
+      done: true
+    })));
+  };
+
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      },
+    ]);
+  };
+
   return (
-    <Container>
+    <Container className="container">
       <Header title="Lista zadań" />
-      <Section title="Dodaj nowe zadanie" body={<Form />} />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form addNewTask={addNewTask} />}
+      />
       <Section
         title="Lista zadań"
-        body={<Tasks tasks={tasks} hideDone={hideDone} removeTask={removeTask} />}
-        extraHeaderContent={
-          <Buttons 
-          tasks={tasks} 
-          hideDone={hideDone} 
-          toggleHideDone={toggleHideDone}
+        body={
+          <Tasks
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
           />
+        }
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
+          />
+
         }
       />
     </Container>
   );
 }
+
 export default App;
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,25 +1,25 @@
 import "./style.css";
-const Tasks = ({ tasks, hideDone, removeTask }) => (
-  <ul className="tasks">
-    {tasks.map((task) => (
-      <li
-        className={`tasks__item ${
-          task.done && hideDone ? " list__item--hidden" : ""
-        }`}
-      >
-        <button className="tasks__button tasks__button--toggleDone ">
+
+const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
+    <ul className="tasks">
+        {tasks.map(task => (
+            <li
+                key={task.id}
+                className={`tasks__item${task.done && hideDone ? " tasks__item--hidden" : ""}`}
+            >
+                <button
+                    className="tasks__button tasks__button--toggleDone"
+                    onClick={() => toggleTaskDone(task.id)}
+                >
           {task.done ? "✔" : ""}
-        </button>
-        <span
-          className={`tasks__content${
-            task.done ? " tasks__content--done" : ""
-          }`}
-        >
-          {task.content}{" "}
-        </span>
-        <button className="tasks__button tasks__button--remove "
-        onClick={() => removeTask(task.id)}
-        >🗑</button>
+          </button>
+                <span className={`tasks__content${task.done ? " tasks__content--done" : ""}`}>
+                    {task.content}
+                </span>
+                <button
+                    className="tasks_button tasks__button--remove"
+                    onClick={() => removeTask(task.id)}
+                >🗑</button>
       </li>
     ))}
   </ul>
